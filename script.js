276a7702b5ea7673530aa3f145ce48945e3bb372
Chris Soncrant <christopher.soncrant@gmail.com>
2022-04-12 18:56:16 -0700

    created button functionality

--- script.js
+++ script.js
@@ -1,2 +1,56 @@
-const twitterBtn = document.querySelector('#twitter');
-const quoteBtn = document.querySelector('#new-quote');
+const twitterBtn = document.getElementById('twitter');
+const quoteBtn = document.getElementById('new-quote');
+
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+
+let apiQuotes = [];
+
+function fillAuthor(author) {
+    if (!author) {
+        authorText.textContent = `- unknown`;
+    } else authorText.textContent = `- ${author}`;
+}
+
+function adjustQuoteFontSize(text) {
+    if (text.length > 90) {
+        console.log(text.length)
+        quoteText.classList.add('long-quote');
+        console.log(quoteText)
+    } else quoteText.classList.remove('long-quote');
+}
+
+function populateQuote(text, author) {
+    adjustQuoteFontSize(text);
+    quoteText.textContent = text;
+    fillAuthor(author);
+}
+
+function newQuote(quoteArray) {
+    const randomNumber = Math.floor(Math.random() * quoteArray.length);
+    const { text, author } = quoteArray[randomNumber];
+    populateQuote(text, author);
+}
+
+async function getQuotes() {
+    const API_URL = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(API_URL);
+        apiQuotes = await response.json();
+        newQuote(apiQuotes);
+    } catch(err) {
+        console.error(err)
+    }
+}
+
+getQuotes();
+
+quoteBtn.addEventListener('click', getQuotes);
+
+function tweetQuote() {
+    let string = `${quoteText.textContent} - ${authorText.textContent}`;
+    const twitterURL = `https://twitter.com/intent/tweet?text=${string}`;
+    window.open(twitterURL, '_blank') 
+}
+
+twitterBtn.addEventListener('click', tweetQuote);
